890353edd3dfa9cf40cf0d20d56dc43021726e29
kcorrai <yanlizcakaan@gmail.com>
2024-01-29 19:41:02 +0300

    e

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from password_gen import generate_password
 
 app = Flask(__name__)
 
@@ -8,4 +9,30 @@ def index():
 
 @app.route('/password-generate', methods=['GET'])
 def password():
-    has_letter = request.get
+    #veriler burada
+    pwd_length = request.form.get('length')
+    print(pwd_length)
+    has_letter = request.form.get('letter')
+    has_digit = request.form.get('digit')
+    has_punc = request.form.get('punctuation')
+    
+    return render_template('password-generator.html',
+                           pwd_length=pwd_length,
+                           has_letter=has_letter,
+                           has_digit=has_digit,
+                           has_punc=has_punc)
+    
+@app.route('/password-generate/result')
+def result(has_letter, has_digit, has_punc, pwd_length):
+    return render_template('password-result.html',
+                           result=generate_password(has_letter,
+                                                    has_digit,
+                                                    has_punc,
+                                                    int(pwd_length)))
+                           
+app.run(debug=True)
+
+    
+
+    
+    
---
+++ password_gen.py
@@ -0,0 +1,22 @@
+import random
+import string
+
+letter = string.ascii_letters
+digits = string.digits
+punctuations = string.punctuation
+
+def generate_password(has_letter, has_digit, has_punc, pwd_length=10):
+    allpwd = []
+    if has_letter:
+        allpwd += letter
+    if has_digit:
+        allpwd += digits
+    if has_punc:
+        allpwd += punctuations
+    
+    pwd = ""
+    
+    for _ in range(pwd_length):
+        pwd += random.choice(allpwd)
+        return pwd
+        
