1154e1f753ad101360beb1c34081259b68ea5142
kcorrai <yanlizcakaan@gmail.com>
2024-01-29 18:59:11 +0300

    backend1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/password-generate', methods=['GET'])
+def password():
+    has_letter = request.get
--- values.py
+++
@@ -1,5 +0,0 @@
-import string
-
-letters = string.ascii_letters
-punctuations = string.punctuation
-digits = string.digits
